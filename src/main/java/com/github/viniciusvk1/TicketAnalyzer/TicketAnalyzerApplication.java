31f46707ba85131318cacf3421752c7fd6bf2f77
vinicius <viniciusalmeida.vk1@gmail.com>
2025-01-28 22:23:49 -0300

    initial files

---
+++ src/main/java/com/github/viniciusvk1/TicketAnalyzer/TicketAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciusvk1.TicketAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/viniciusvk1/TicketAnalyzer/TicketAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viniciusvk1.TicketAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
