02d04b9dacdae791ac272efb994a97633e6818aa
vinicius <viniciusalmeida.vk1@gmail.com>
2025-01-28 22:40:25 -0300

    ticket service

---
+++ src/main/java/com/github/viniciusvk1/TicketAnalyzer/service/TicketService.java
@@ -0,0 +1,34 @@
+package com.github.viniciusvk1.TicketAnalyzer.service;
+
+import com.github.viniciusvk1.TicketAnalyzer.model.Ticket;
+import com.github.viniciusvk1.TicketAnalyzer.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class TicketService {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public List<Ticket> getTicketsByStatus(String status) {
+        return ticketRepository.findByStatusTicket(status);
+    }
+    public Map<String, Long> getTicketsByDeveloperStatusCount(String nomeDesenvolvedor) {
+        List<Ticket> tickets = ticketRepository.findByNomeDesenvolvedor(nomeDesenvolvedor);
+        return tickets.stream()
+                .collect(Collectors.groupingBy(Ticket::getStatusDesenvolvimento, Collectors.counting()));
+    }
+
+    public List<Ticket> getTicketsByDeveloper(String nomeDesenvolvedor) {
+        return ticketRepository.findByNomeDesenvolvedor(nomeDesenvolvedor);
+    }
+
+    public List<Ticket> getTicketsByPriority(String prioridade) {
+        return ticketRepository.findByPrioridade(prioridade);
+    }
+}
